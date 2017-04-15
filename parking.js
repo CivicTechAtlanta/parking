2b3be39fa4944b1a3108e0b0bc1709c1500552f8
Luigi Ray-Montanez <luigi.montanez@gmail.com>
2017-04-15 15:43:52 -0400

    Process array of rows and create the API object

--- parking.js
+++ parking.js
@@ -1,7 +1,60 @@
 const scrapeIt = require("scrape-it");
 const moment = require('moment-timezone');
+const _ = require('lodash');
+const util = require('util')
 
 const dateRegEx = new RegExp(/: (.*)/);
+
+function cleanUp(page) {
+    var ignore = ['Station', 'Parking Status'];
+
+    var names =  [
+         'North Springs'
+       , 'Sandy Springs'
+       , 'Dunwoody'
+       , 'Doraville'
+       , 'Chamblee'
+       , 'Brookhaven'
+       , 'Lenox'
+       , 'Lindbergh Center'
+       , 'College Park'
+       , 'Kensington' ];
+
+    stations = [];
+    for (i = 0; i < page.rows.length - 1; i++) {
+      row = page.rows[i];
+      if (_.includes(ignore, row)) {
+          // do nothing
+      } else if (_.includes(names, row)) {
+          stations.push({
+              name: row
+            , lots: []
+          });
+      } else {
+          var lots = _.last(stations).lots
+          if (_.includes(_.toLower(row), 'open')) {
+              lots.push({
+                  status: 'open'
+                , label: row
+              })
+          } else if (_.includes(_.toLower(row), 'full')) {
+              lots.push({
+                  status: 'full'
+                , label: row
+              })
+          } else {
+            // do nothing
+          }
+      }
+    }
+
+    var api = {
+        lastUpdated: page.lastUpdated
+      , stations: stations
+    };
+
+    return api;
+}
 
 // Callback interface
 scrapeIt("http://www.itsmarta.com/parking.aspx", {
@@ -13,7 +66,7 @@ scrapeIt("http://www.itsmarta.com/parking.aspx", {
           return moment.tz(str, 'M/DD/YYYY h:mmA', "America/New_York").toLocaleString();
         }
     },
-    stations: {
+    rows: {
         listItem: "#ctl00_ContentPlaceHolder1_cntpolice > div > div > div > div > table:nth-child(5) > tbody > tr > td"
       , convert: function(x) {
         return x.trim();
@@ -21,5 +74,6 @@ scrapeIt("http://www.itsmarta.com/parking.aspx", {
     }
 
 }, (err, page) => {
-    console.log(err || page);
+    var output = cleanUp(page);
+    console.log(util.inspect(output, false, null))
 });
